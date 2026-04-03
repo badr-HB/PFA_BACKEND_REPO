6b955195900b1b38cbc591c8917e28ac72664f85
badr <badrsama300@gmail.com>
2026-04-03 21:11:40 +0100

    update

--- backend/controllers/ApplicationControllers.js
+++ backend/controllers/ApplicationControllers.js
@@ -34,9 +34,74 @@ export const SendJoinResquest = async (req, res) => {
             userId: userId
         }
     })
-    return res.status(200).json({ message: `you got a join request from ${UserExists.displayname} to the project ${IdeaExists.Title}` })
+    return res.status(200).json({ message: "success" })
 }
 
 export const JoinRequest = async (req, res) => {
-    //here admin can accept or ignore the requests
+    const { data } = req.body;
+    const { joinid } = req.params;
+    const adminId = req.user.id;
+
+    const checkauth = await prisma.idea.findFirst({
+        where: {
+            ownerId: adminId
+        }
+    })
+    const checkrequest = await prisma.joinrequest.findFirst({
+        where: {
+            id: joinid,
+            ideaId: checkauth.id
+        }
+    })
+    if (!checkrequest && !checkauth) {
+        return res.status(400).json({ message: "something went wrong please try again" })
+    }
+
+    if (data === "accepted") {
+        const accept = await prisma.joinrequest.update({
+            where: {
+                id: joinid
+            },
+            data: {
+                status: "accepted"
+            }
+        })
+        return res.status(200).json({ message: "request has been approved" })
+    }
+    else if (data === "rejected") {
+        const reject = await prisma.joinrequest.update({
+            where: {
+                id: joinid
+            },
+            data: {
+                status: "rejected"
+            }
+        })
+        return res.status(200).json({ message: "request has been ignored" })
+    }
+}
+
+export const join_notification = async (req, res) => {
+    const id = req.user.id;
+    const { projectid } = req.params;
+
+    const checkadmin = await prisma.idea.findFirst({
+        where: {
+            ownerId: id,
+            id: projectid
+        }
+    })
+
+    if (!checkadmin) {
+        return res.status(400).json({ message: "something went wrong" })
+    }
+
+    const getposts = await prisma.joinrequest.findMany({
+        where: {
+            ideaId: projectid
+        }
+    })
+    return res.status(200).json({ message: getposts })
+
+
 }
--- backend/routes/ApplicationsRoutes.js
+++ backend/routes/ApplicationsRoutes.js
@@ -1,10 +1,11 @@
 import express from  "express";
 const Route = express.Router();
-import { SendJoinResquest } from "../controllers/ApplicationControllers.js";
+import { SendJoinResquest,JoinRequest,join_notification } from "../controllers/ApplicationControllers.js";
 import { cookies } from "../middlewares/cookies.js";
 
 
-Route.post('/application/:projectid',cookies,SendJoinResquest)
-
+Route.post('/application/:projectid/join-request',cookies,SendJoinResquest)
+Route.post('/application/:joinid/handle-request',cookies,JoinRequest)
+Route.get('/application/requests/:projectid',cookies,join_notification)
 
 export default Route;
